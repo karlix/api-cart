a63c6067600ffa32fd83cd8e983453b4e492b9be
karlix <carles.blanch87@gmail.com>
2024-02-28 18:30:45 +0100

    create database migrations

---
+++ database/migrations/2024_02_22_120308_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name')->nullable(false);
+            $table->unsignedDecimal('price', 8, 2)->default(0)->nullable(false);
+            $table->unsignedInteger('stock')->default(0)->nullable(false);
+            $table->text('image')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2024_02_22_120547_create_cart_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('cart_id')->constrained()->cascadeOnUpdate();
+            $table->foreignUuid('product_id')->constrained()->cascadeOnUpdate();
+            $table->unsignedInteger('quantity')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
