f54af47d97593e7b55594c73b12a3c41c84aaa43
karlix <carles.blanch87@gmail.com>
2024-02-28 21:06:04 +0100

    create api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/cart/{idCart}/total-products', "App\Http\Controllers\Api\CartController@getTotalProducts")->name("cart.total");
+Route::get('/cart/{idCart}', "App\Http\Controllers\Api\CartController@getCart")->name("cart.get");
+
+Route::put('/cart/{idCart}/add', "App\Http\Controllers\Api\CartController@addToCart")->name("cart.add");
+Route::put('/cart/{idCart}/remove', "App\Http\Controllers\Api\CartController@removeFromCart")->name("cart.remove");
+Route::put('/cart/{idCart}/clear', "App\Http\Controllers\Api\CartController@clear")->name("cart.clear");
+Route::put('/cart/{idCart}/confirm', "App\Http\Controllers\Api\CartController@confirmPurchase")->name("cart.confirm");
+
+Route::delete('/cart/{idCart}', "App\Http\Controllers\Api\CartController@delete")->name("cart.delete");
+
+Route::post('/cart', "App\Http\Controllers\Api\CartController@create")->name("cart.create");
