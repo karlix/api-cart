ecaa5a79aefdcff2a63af35c3f875adb30cfb79e
karlix <carles.blanch87@gmail.com>
2024-02-28 18:59:56 +0100

    Create controller implementation to delete cart

---
+++ src/Cart/Infrastructure/DeleteCartController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace src\Cart\Infrastructure;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use src\Cart\Application\CreateCartUseCase;
+use src\Cart\Application\DeleteCartUseCase;
+use src\Cart\Domain\ValueObjects\CartId;
+use src\Cart\Infrastructure\Repositories\EloquentCartRepository;
+
+class DeleteCartController
+{
+    private EloquentCartRepository $repository;
+
+    public function __construct(EloquentCartRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function __invoke(Request $request)
+    {
+        $validator = Validator::make($request->route()->parameters(), [
+            'idCart' => 'required|uuid'
+        ],[
+            'idCart.required' => 'The idCart is required',
+            'idCart.uuid' => 'The idCart must be a valid UUID'
+        ]);
+        $validatedData = $validator->validate();
+
+        $idCart = $validatedData['idCart'];
+
+        $deleteCartUseCase = new DeleteCartUseCase($this->repository);
+        $deleteCartUseCase->__invoke($idCart);
+    }
+
+}
